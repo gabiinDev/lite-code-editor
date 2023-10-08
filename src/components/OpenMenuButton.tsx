faee43155754c14e94e3b1020ed7ccf77476f737
gabiinDev <gabriel@gabrieldiazdev.com>
2023-10-08 10:52:59 -0300

    adding resize bar image for background and fix loading problem with loading screen when editors are not loaded yet

--- src/components/OpenMenuButton.tsx
+++ src/components/OpenMenuButton.tsx
@@ -9,10 +9,7 @@ const OpenMenuButton = ({ onClick }: Props) => {
 		<button
 			class='text-white bg-neutral-600 opacity-90 hover:opacity-80 rounded-full shadow-md shadow-black'
 			type='button'
-			data-drawer-target='drawer-top-example'
-			data-drawer-show='drawer-top-example'
 			data-drawer-placement='top'
-			aria-controls='drawer-top-example'
 			title={'Open menu'}
 			onClick={onClick}
 		>
